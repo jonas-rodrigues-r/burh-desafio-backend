4aee7942d22c7cbe26209c0761099ea9f1752fdc
Jonas Rodrigues Ramires <jonasrodriguesramires@gmail.com>
2022-12-16 23:55:28 -0300

    Adicionando validacoes do formulario de companies

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -17,4 +17,23 @@ class Company extends Model
         'cnpj',
         'id_plan',
     ];
+
+    public static function createRules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:100',
+            'description' => 'required|string|min:3|max:250',
+            'cnpj' => 'required|numeric|min:14|max:14',
+            'id_plan' => 'required|numeric',
+        ];
+    }
+
+    public static function updateRules(): array
+    {
+        return [
+            'name' => 'string|min:3|max:100',
+            'description' => 'string|min:3|max:250',
+            'id_plan' => 'numeric',
+        ];
+    }
 }
