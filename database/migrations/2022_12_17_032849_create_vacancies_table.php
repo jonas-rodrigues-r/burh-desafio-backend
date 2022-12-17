723ccea58b70c7c022257e7667135a8be0671ffc
Jonas Rodrigues Ramires <jonasrodriguesramires@gmail.com>
2022-12-17 01:10:15 -0300

    Adicionando a migration de criacao da tabela vacancies

---
+++ database/migrations/2022_12_17_032849_create_vacancies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('type', 100);
+            $table->decimal('wage', 10, 2);
+            $table->unsignedBigInteger('id_company');
+            $table->foreign('id_company')->references('id')->on('companies')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacancies');
+    }
+};
