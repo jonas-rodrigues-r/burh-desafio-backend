b7519c4b658e8cdf99fbb6242004c80a018d5f45
Jonas Rodrigues Ramires <jonasrodriguesramires@gmail.com>
2022-12-16 23:52:54 -0300

    Adicionando migration de criacao da tabela companies

---
+++ database/migrations/2022_12_17_024833_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('description', 250);
+            $table->string('cnpj')->unique();
+            $table->unsignedBigInteger('id_plan');
+            $table->foreign('id_plan')->references('id')->on('plans')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
